39daa62f9c75d4b92da9c4acf9987eaa465437d4
hermaouss <139243884+hermaouss@users.noreply.github.com>
2023-08-03 02:53:01 +0300

    Add files via upload

---
+++ HavaEtkinlik.java
@@ -0,0 +1,20 @@
+package Giris;
+import java.util.Scanner;
+public class HavaEtkinlik {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sicaklik;
+        System.out.println("Sıcaklık giriniz: ");
+        sicaklik = scan.nextInt();
+
+        if(sicaklik < 5){
+            System.out.println("Kayak yapabilirsiniz");
+        } else if (sicaklik >= 5 && sicaklik <=15) {
+            System.out.println("Sinemaya gidebilirsiniz");
+        } else if (sicaklik > 15 && sicaklik <=25) {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        } else if (sicaklik > 25) {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+    }
+}
